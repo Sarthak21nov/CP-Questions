df0e0a226a7c0c7d8f72a7ca7d67aeaaa3bf8577
Sarthak Yash Srivastava <115006366+Sarthak21nov@users.noreply.github.com>
2024-12-02 17:05:33 +0530

    Create FlattenLinkedList.java

---
+++ FlattenLinkedList.java
@@ -0,0 +1,21 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class FlattenLL {
+    // Function to flatten a linked list
+    Queue<Node> q = new LinkedList<>();
+    Node flatten(Node root) {
+        // code here
+        Node current = root;
+        while(current != null){
+            if(current.bottom != null){
+                q.add(current.bottom);
+            }
+            
+            if(current.next == null){
+                current.next = q.poll();
+            }
+            current = current.next;
+        }
+    }
+}
